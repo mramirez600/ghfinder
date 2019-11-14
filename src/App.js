346c9e15be0b60df0502d13a2f2c61aa23c01db2
Marvin Ramirez <eldo@hotmail.com>
2019-11-13 20:33:55 -0500

    minor update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import "./App.css";
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
 
 class App extends Component {
   render() {
-    const name = "john doe";
+    const name = 'john doe';
     return (
       <div className="navbar bg-primary">
         <Navbar />
+        <h1>Hello</h1>
       </div>
     );
   }
